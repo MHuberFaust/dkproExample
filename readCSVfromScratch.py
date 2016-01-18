cbe1686f5ff10cf8b84e9fca9f1fd4f3d1a44e3a
MHuberFaust <michael.huber@stud-mail.uni-wuerzburg.de>
2016-01-18 14:30:17 +0100

    read as dict; setup for drawing graphs

--- readCSVfromScratch.py
+++ readCSVfromScratch.py
@@ -8,10 +8,20 @@ import csv
 #POS-Tag: row[9]
 #NamedEntity: row[14]
 
+'''
 with open ('/Users/MHuber/Documents/Dariah/dariah-dkpro-wrapper-0.4.2/Abraham.csv', encoding='utf-8') as csvfile:
     readCSV = csv.reader(csvfile, delimiter='\t')
-
+    print (readCSV)
     for row in readCSV:
         if row[14]!= "_":
         #print(row)
             print(row[7],row[9],row[14] )
+
+            
+'''
+with open ('/Users/MHuber/Documents/Dariah/dariah-dkpro-wrapper-0.4.2/Abraham.csv', encoding='utf-8') as csvfile:
+    readCSV = csv.DictReader(csvfile, delimiter='\t')
+
+    for row in readCSV:
+        if row['NamedEntity']!="_" and row['CPOS']!="PUNC":
+            print (row['Lemma'], row['POS'],row['NamedEntity'])
