850765edce4ac4bb87b4869da63076bcae6a0761
MHuberFaust <michael.huber@stud-mail.uni-wuerzburg.de>
2016-01-18 12:29:55 +0100

    prints lemma, Pos-Tag and NamedEntity now; setup for drawing graphs

--- readCSVfromScratch.py
+++ readCSVfromScratch.py
@@ -5,9 +5,13 @@ author: MHuber
 '''
 import csv
 
+#POS-Tag: row[9]
+#NamedEntity: row[14]
+
 with open ('/Users/MHuber/Documents/Dariah/dariah-dkpro-wrapper-0.4.2/Abraham.csv', encoding='utf-8') as csvfile:
     readCSV = csv.reader(csvfile, delimiter='\t')
+
     for row in readCSV:
-        print(row)
-        print(row[1])
-        print(row[2])
+        if row[14]!= "_":
+        #print(row)
+            print(row[7],row[9],row[14] )
