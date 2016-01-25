8c8700b74bf966657388f87f95ea70da5d0ba8e3
MHuberFaust <michael.huber@stud-mail.uni-wuerzburg.de>
2016-01-25 11:34:50 +0100

    creates dict with lemma:countoflemma as a prep for drawing graphs

--- readCSVfromScratch.py
+++ readCSVfromScratch.py
@@ -3,11 +3,22 @@ Alternative way of reading the csv-File produced by the dproWrapper version 0.43
 author: MHuber
 
 '''
-import csv
+
+#Schleife bauen, die B-per und I-Per zusammenfasst und in einem dict ablegt
+#
+#Vorannahme, dass Personennamen im Text zumindest durch ein Satzzeichen getrennt sind
+#nur eine Kombination aus B-Per und mindeste 1 I-Per wird als Name gespeichert
+##wegen: "Matthäus Megerle und dessen Frau Ursula ->B-BPer (geborene Wagner->BPer)"
+
+#für jeden Text eine Datei in einem Graphendatenformat abspeichern? o. on the fly generieren?
+
+
+
 
 #POS-Tag: row[9]
 #NamedEntity: row[14]
-
+import csv
+from collections import defaultdict
 '''
 with open ('/Users/MHuber/Documents/Dariah/dariah-dkpro-wrapper-0.4.2/Abraham.csv', encoding='utf-8') as csvfile:
     readCSV = csv.reader(csvfile, delimiter='\t')
@@ -21,7 +32,19 @@ with open ('/Users/MHuber/Documents/Dariah/dariah-dkpro-wrapper-0.4.2/Abraham.cs
 '''
 with open ('/Users/MHuber/Documents/Dariah/dariah-dkpro-wrapper-0.4.2/Abraham.csv', encoding='utf-8') as csvfile:
     readCSV = csv.DictReader(csvfile, delimiter='\t')
-
+    lemma =[]
+    necounter=defaultdict(int)
     for row in readCSV:
         if row['NamedEntity']!="_" and row['CPOS']!="PUNC":
+            lemma.append(row['Lemma'])
             print (row['Lemma'], row['POS'],row['NamedEntity'])
+        else:
+            #fill dict
+            if lemma:
+                joinedLemma=' '.join(lemma)
+                print("this is the Lemma "+joinedLemma)
+                necounter[joinedLemma]+=1
+                lemma=[]
+            
+    print(necounter['Sancta Clara'])
+                
