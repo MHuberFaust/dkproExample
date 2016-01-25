0ec8d9ada8a1e3f97d5bd5f6567cb0d1bcf3d745
MHuberFaust <michael.huber@stud-mail.uni-wuerzburg.de>
2016-01-25 15:14:27 +0100

    graph creating functions added to readCSVfromScratch; not yet tested

--- readCSVfromScratch.py
+++ readCSVfromScratch.py
@@ -13,7 +13,13 @@ author: MHuber
 #für jeden Text eine Datei in einem Graphendatenformat abspeichern? o. on the fly generieren?
 #Gewichtungen sollten bestenfalls gleich hier vorgenommen werden
 
+'''
+Weitere Vorgehensweise:
+    1. für jedes dict einen Knoten erstellen
+    2. dicts vergleichen, bei Match Kante von dict zu dict erstellen
+    3. Graph zeichnen
 
+'''
 
 #POS-Tag: row[9]
 #NamedEntity: row[14]
@@ -30,21 +36,30 @@ with open ('/Users/MHuber/Documents/Dariah/dariah-dkpro-wrapper-0.4.2/Abraham.cs
 
             
 '''
-with open ('/Users/MHuber/Documents/Dariah/dariah-dkpro-wrapper-0.4.2/Abraham.csv', encoding='utf-8') as csvfile:
-    readCSV = csv.DictReader(csvfile, delimiter='\t')
-    lemma =[]
+
+
+def neCount(inputfile):
     necounter=defaultdict(int)
-    for row in readCSV:
-        if row['NamedEntity']!="_" and row['CPOS']!="PUNC":
-            lemma.append(row['Lemma'])
-            print (row['Lemma'], row['POS'],row['NamedEntity'])
-        else:
-            #fill dict
-            if lemma:
-                joinedLemma=' '.join(lemma)
-                print("this is the Lemma "+joinedLemma)
-                necounter[joinedLemma]+=1
-                lemma=[]
-            
-    print(necounter['Sancta Clara'])
+    with open (inputfile, encoding='utf-8') as csvfile:
+        readCSV = csv.DictReader(csvfile, delimiter='\t')
+        lemma =[]
+        
+        for row in readCSV:
+            if row['NamedEntity']!="_" and row['CPOS']!="PUNC":
+                lemma.append(row['Lemma'])
+                print (row['Lemma'], row['POS'],row['NamedEntity'])
+            else:
+                #fill dict
+                if lemma:
+                    joinedLemma=' '.join(lemma)
+                    print("this is the Lemma "+joinedLemma)
+                    necounter[joinedLemma]+=1
+                    lemma=[]
                 
+        #print(necounter['Sancta Clara'])
+    #print(necounter['Sancta Clara'])
+    return necounter
+          
+          
+          
+neCount("/Users/MHuber/Documents/Dariah/dariah-dkpro-wrapper-0.4.2/Abraham.csv")      
